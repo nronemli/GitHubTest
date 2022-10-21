1ba8db1529097ea6bb16f85d476d598b072fb7c3
SaidBayraktar <bayraktarsaid1@gmail.com>
2022-10-21 21:17:22 +0100

    test second

--- src/test/java/saidsPackage/TestingSaid.java
+++ src/test/java/saidsPackage/TestingSaid.java
@@ -3,5 +3,6 @@ package saidsPackage;
 public class TestingSaid {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello two");
     }
 }
